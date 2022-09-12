14ffa3cf7c62e7b648441287f165fb946e1129ca
elisas17 <70175096+elisas17@users.noreply.github.com>
2022-09-12 15:35:40 -0300

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_GET["resposta"]) && 
+!empty($_GET["resposta"]) && isset($_GET["mensagem"]) && !empty($_GET["mensagem"])
+
+$reposta = $_GET["resposta"]; 
+$mensagem = $_GET["mensagem"];
+
+if(isset($_GET["acao"]) && !empty($_GET["acao"]));
+
+$resgatarProduto = $produto->carregarProduto($id);
+
+?>
